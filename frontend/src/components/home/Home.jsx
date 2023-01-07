f931519331b6c9033cb3a9fee1e8b8187938f6ee
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-01-06 21:57:17 -0300

    Created content component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capitulo React.">
+        <div className='display-4'>Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar  construção de um cadastro de Usúarios</p>
+    </Main>
